52d50b724d959f63d32f800f7a304171ff08bdc1
Mateo-EV <riojamatthew@gmail.com>
2024-07-14 21:13:07 -0500

    test3

--- src/controllers/VehicleController.js
+++ src/controllers/VehicleController.js
@@ -17,8 +17,7 @@ const getCaptchaImage = async (req, res) => {
   const plate = req.params.plate
 
   const browser = await puppeteer.launch({
-    headless: false,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    headless: true
   })
   const page = await browser.newPage()
 
